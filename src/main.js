9e04a6c74fa3393b8ec364de0dd68cc7767bf4ae
Niklas <nm4500@rhoen.de>
2017-07-06 20:37:01 +0200

    Set swift version in config.xml

--- src/main.js
+++ src/main.js
@@ -141,6 +141,10 @@ export default (context) => {
               if (config.getPreference('UseLegacySwiftLanguageVersion', 'ios')) {
                 xcodeProject.updateBuildProperty('SWIFT_VERSION', '2.3', buildConfig.name);
                 console.log('Use legacy Swift language version', buildConfig.name);
+              } else if (config.getPreference('UseSwiftLanguageVersion', 'ios')) {
+                const swiftVersion = config.getPreference('UseSwiftLanguageVersion', 'ios');
+                xcodeProject.updateBuildProperty('SWIFT_VERSION', swiftVersion, buildConfig.name);
+                console.log('Use Swift language version', swiftVersion);
               } else {
                 xcodeProject.updateBuildProperty('SWIFT_VERSION', '3.0', buildConfig.name);
                 console.log('Update SWIFT version to 3.0', buildConfig.name);
